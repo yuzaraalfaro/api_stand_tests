c4d4eab80bd8ef4d190036e58c0e5a76c34926e7
yuzaraalfaro <yuzara09@gmail.com>
2024-04-22 21:26:47 -0500

    Prueba 2

--- create_user_test.py
+++ create_user_test.py
@@ -9,7 +9,7 @@ def get_user_body(first_name):
     current_body["firstName"] = first_name
     # Se devuelve un nuevo diccionario con el valor firstName requerido
     return current_body
-
+#probando commit and push
 # Función de prueba positiva
 def positive_assert(first_name):
     # El cuerpo actualizado de la solicitud se guarda en la variable user_body
